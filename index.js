a94247afdb314e1fb61bf6fbe42466780fb28da6
Uzair Nasir <uxairjan3@gmail.com>
2023-11-18 11:41:44 +0500

    js basic lec

---
+++ index.js
@@ -0,0 +1,28 @@
+var num = 10; 
+var value=1.0;
+var name = 'uzair';
+var isValid = true;
+var array = [1,"2",true,10.6,[], {
+    id: 1,
+    name: "Zohaib",
+    post: {
+        name: "Cartoon"
+    }
+}];
+var object = {
+    id: 1,
+    name: "Zohaib",
+    post: {
+        name: "Cartoon"
+    }
+}
+
+console.log(array[3], object.name, object.post.name)
+// console.log(typeof num, typeof value, typeof name, typeof isValid, typeof array, typeof object)
+
+var addition = num/2 + num * value; 
+// console.log(addition)
+// console.log(9 === '9')
+console.log(num >= 1 && num <= 10)
+// console.log(9 >= 9)
+// console.log(9 !== 10)
