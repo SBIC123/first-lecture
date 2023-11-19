4f60742732c08fdc6ac312f747b91b691a5f1e52
Uzair Nasir <uxairjan3@gmail.com>
2023-11-19 12:04:50 +0500

    conditionals id else

--- index.js
+++ index.js
@@ -1,28 +1,68 @@
-var num = 10; 
-var value=1.0;
-var name = 'uzair';
-var isValid = true;
-var array = [1,"2",true,10.6,[], {
+var num = 11;
+var value = 1.0;
+var name = "Aizaz";
+var array = [
+  1,
+  "2",
+  true,
+  10.6,
+  [],
+  {
     id: 1,
     name: "Zohaib",
     post: {
-        name: "Cartoon"
-    }
-}];
+      name: "Cartoon",
+    },
+  },
+];
 var object = {
-    id: 1,
-    name: "Zohaib",
-    post: {
-        name: "Cartoon"
+  id: 1,
+  name: "Zohaib",
+  post: {
+    name: "Cartoon",
+  },
+};
+
+// console.log(array[3], object.name, object.post.name)
+// console.log(typeof num, typeof value, typeof name, typeof isValid, typeof array, typeof object)
+
+var addition = num / 2 + num * value;
+// console.log(addition)
+// console.log(9 === '9')
+// console.log(num >= 1 && num <= 10)
+// console.log(9 >= 9)
+// console.log(9 !== 10)
+
+if (num < 10) {
+  console.log("This niumber is smaller than 10");
+}
+if (num === 10) {
+  console.log("Numbe is equal to 10");
+}
+
+name === "Uzair" ? console.log("Teacher") : console.log("Student");
+
+var numCheck = num % 2 === 0
+  ? `${num} is Even Number`
+  : `${num} is Odd Number`;
+  console.log(numCheck)
+
+var isValid = true;
+var output = !isValid;
+console.log(output)
+
+var num2 = 7;
+if(num2 < 10 && num2 < 5) {
+        console.log("smaller than 5")
+}
+else {
+    if(num2 < 10 && num2 > 5){
+            console.log("greater than 5")
+    }
+    else {
+    console.log("Num is invalid")
     }
 }
 
-console.log(array[3], object.name, object.post.name)
-// console.log(typeof num, typeof value, typeof name, typeof isValid, typeof array, typeof object)
-
-var addition = num/2 + num * value; 
-// console.log(addition)
-// console.log(9 === '9')
-console.log(num >= 1 && num <= 10)
-// console.log(9 >= 9)
-// console.log(9 !== 10)
+const num2Check = num2 < 10 && num2 < 5 ? "smaller than 5" : num2 < 10 && num2 > 5 ? "greater than 5" : "Num is invalid"
+console.log(num2Check)
