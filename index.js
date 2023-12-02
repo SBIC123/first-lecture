e8acd4d25228125aa627ee6575b3911bd988cb07
Uzair Nasir <uxairjan3@gmail.com>
2023-12-02 11:30:41 +0500

    functions and loops

--- index.js
+++ index.js
@@ -67,19 +67,19 @@
 // const num2Check = num2 < 10 && num2 < 5 ? "smaller than 5" : num2 < 10 && num2 > 5 ? "greater than 5" : "Num is invalid"
 // console.log(num2Check)
 
-var number = "true";
-if(typeof number === "number") {
-  console.log(`${number} is of type number`)
-}
-else if(typeof number === "string") {
-  console.log(`${number} is of type string`)
-}
-else {
-  console.log(`${number} is of type boolean`)
-}
+// var number = "true";
+// if(typeof number === "number") {
+//   console.log(`${number} is of type number`)
+// }
+// else if(typeof number === "string") {
+//   console.log(`${number} is of type string`)
+// }
+// else {
+//   console.log(`${number} is of type boolean`)
+// }
 
-var color = "blue";
-var a = 5, b=10, operator="a";
+// var color = "blue";
+// var a = 5, b=10, operator="a";
 
 // if(operator === "+")
 //   console.log("a+b=",a+b)
@@ -88,19 +88,61 @@ var a = 5, b=10, operator="a";
 // else
 //   console.log("a*b=",a*b)
 
-switch(operator) {
-  case '+': 
-    console.log("a+b=",a+b)
-    break;
-  case '-': 
-    console.log("a-b=",a-b)
-    break;
-  case '/': 
-    console.log("a/b=",a/b)
-    break;
-  case '*': 
-    console.log("a*b=",a*b)
-    break;
-  default: 
-    console.log("Please ennter correct operator value");
+// switch(operator) {
+//   case '+': 
+//     console.log("a+b=",a+b)
+//     break;
+//   case '-': 
+//     console.log("a-b=",a-b)
+//     break;
+//   case '/': 
+//     console.log("a/b=",a/b)
+//     break;
+//   case '*': 
+//     console.log("a*b=",a*b)
+//     break;
+//   default: 
+//     console.log("Please ennter correct operator value");
+// }
+
+// for(var i=0; i<=9; i++)
+// console.log("hello i am uzair")
+// var j=0;
+// while(j<=9) {
+//   console.log(j+"while loop");
+//   j++
+// }
+
+// var k=10;
+// do {
+//   console.log(k+" do while loop");
+//   k++
+// }
+// while(k<=9)
+
+
+
+var array = [1, 2, 3, 4, 5];
+// var sum = (num) => {
+//   for(var i=0; i<array.length; i++)
+//   console.log(`${array[i]} + ${num} = `, array[i]+num);
+//   console.log("******************")
+// }
+
+// sum(5)
+
+// var print = (num) => {
+//   return num * 5;
+// }
+
+// for(var i =0; i<array.length; i++) {
+//   var returnedNum = print(array[i], 8);
+//   console.log(returnedNum);
+// }
+
+var table = (num) => {
+  for(var i=10; i>0; i--) {
+    console.log(`${num} * ${i} = ${num*(i)}`)
+  }
 }
+table(15);
